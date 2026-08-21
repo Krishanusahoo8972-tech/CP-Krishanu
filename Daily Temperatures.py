9a4c2858b7dc161d22933500710bc66a7d3db574
Krishanusahoo8972-tech <krishanusahoo8972@gmail.com>
2026-08-21 11:29:52 +0530

    Delete Daily Temperatures.py

--- Daily Temperatures.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def dailyTemperatures(self, T):
-        ans = [0]*len(T)
-        for i in range(len(T)):
-            for j in range(i+1, len(T)):
-                if T[j] > T[i]:
-                    ans[i] = j-i
-                    break
-        return ans
